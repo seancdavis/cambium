0dc67120020d5d5e7b3b5facc67dd5b68e2e06ee
Sean C Davis <scdavis41@gmail.com>
2014-08-06 14:58:14 -0400

    refs #15 -- add some helpers for installing gems that aren't dependencies

--- lib/generators/cambium/helpers/generators_helper.rb
+++ lib/generators/cambium/helpers/generators_helper.rb
@@ -1,6 +1,16 @@
 module Cambium
   module GeneratorsHelper
 
+    # ------------------------------------------ System Commands / Aliases
+
+    # Shorthand for `bundle exec`
+    # 
+    def be
+      "bundle exec"
+    end
+
+    # Runs a system command, but with pretty output
+    # 
     def run_cmd(cmd, options = {})
       print_table(
         [
@@ -15,18 +25,9 @@ module Cambium
       end
     end
 
-    def template_file(name)
-      File.expand_path("../../templates/#{name}", __FILE__)
-    end
-
-    def file_contents(template)
-      File.read(template_file(template))
-    end
-
-    def be
-      "bundle exec"
-    end
-
+    # Make sure we get a matching answer before moving on. Useful for usernames,
+    # passwords, and other items where user error is harmful (or painful)
+    # 
     def confirm_ask(question)
       answer = ask("\n#{question}")
       match = ask("CONFIRM #{question}")
@@ -36,6 +37,54 @@ module Cambium
         say set_color("Did not match.", :red)
         confirm_ask(question)
       end
+    end
+
+    # ------------------------------------------ Templating
+
+    # Get the path to a particular template
+    # 
+    def template_file(name)
+      File.expand_path("../../templates/#{name}", __FILE__)
+    end
+
+    # Read a template and return its contents
+    # 
+    def file_contents(template)
+      File.read(template_file(template))
+    end
+
+    # ------------------------------------------ Gems
+
+    # Run `bundle install`
+    # 
+    def bundle
+      run_cmd "bundle install"
+    end
+
+    # Get the local path where the gems are installed
+    # 
+    def gem_dir
+      gem_dir = `bundle show rails`
+      gem_dir = gem_dir.split('/')
+      gem_dir[0..-3].join('/')
+    end
+
+    # Add gem to Gemfile
+    # 
+    def add_to_gemfile(name)
+      insert_into_file "Gemfile", "\n\ngem '#{name}'", :after => "rubygems.org'"
+    end
+
+    # First, install the gem in the local gem directory, then add it to the
+    # Gemfile.
+    # 
+    # Note: This will run bundle install after adding the gem, unless specified
+    # otherwise.
+    # 
+    def install_gem(name, bundle? = true)
+      run "gem install #{name} -i #{gem_dir}"
+      add_to_gemfile(name)
+      bundle if bundle?
     end
 
   end
