9b6b358d921e6591cb986c723e0a7dce501be361
Sean C Davis <scdavis41@gmail.com>
2014-08-26 10:40:47 -0400

    a little cleanup on page generator

--- lib/generators/cambium/model/page_generator.rb
+++ lib/generators/cambium/model/page_generator.rb
@@ -35,39 +35,63 @@ module Cambium
         )
       end
 
-      # ------------------------------------------ Add App config
-
+      # Application config
+      # 
       def add_application_config
         application 'config.template_directory = "#{Rails.root}/app/views/page_templates"'
       end
 
-      # ------------------------------------------ Add page templaes
-
+      # Add page templaes
+      # 
       def add_page_templates
         directory('app/views/page_templates','app/views/page_templates')
       end
 
-      # ------------------------------------------ Add Controller Templates
-
+      # Add controller templates
+      # 
       def add_controller_files
         controller_path = "app/controllers/pages_controller.rb"
         template(controller_path, controller_path)
       end
 
-      # ------------------------------------------ Migrate & Annotate
-
+      # Migrate & Annotate
+      # 
       def migrate_and_annotate
         rake "db:migrate"
         run_cmd "#{be} annotate"
       end
 
-      # ------------------------------------------ User output
-      # This is kind of a cop out in leu of appending to the *end* of routes.rb
-      def notes_to_user
-        puts "\n== Add the following to the end of your config/routes.rb file ================="
-        puts "get '/:slug', :to => 'pages#show', :as => 'page'"
-        puts "\n"
+      # Add routes
+      # 
+      def add_routes
+        insert_into_file(
+          "config/routes.rb",
+          "get '/:slug', :to => 'pages#show', :as => 'page'\n\n  ",
+          :before => /root\ (.*)\n/
+        )
+        if File.read(Rails.root.join('config','routes.rb')).include?('/:slug')
+          uncomment_lines(
+            Rails.root.join('config','routes.rb'),
+            /get\ \'\/\:slug/
+          )
+        else
+          add_routes_manually
+        end
       end
+
+      private
+
+        # If we couldn't find a root route in the config file, then we tell the
+        # user to add the routes manually.
+        # 
+        def add_routes_manually
+          say set_color(
+            "\nAdd the following to the end of your config/routes.rb file:\n",
+            :yellow,
+            :bold
+          )
+          say "    get '/:slug', :to => 'pages#show', :as => 'page'\n"
+        end
 
     end
   end
