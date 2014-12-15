74f3cc003a77951773d40c94addcde6613534b0f
Sean C Davis <scdavis41@gmail.com>
2014-12-15 14:33:44 -0500

    add default gemfile based on a couple config items

--- lib/generators/templates/config/initializers/cambium.rb
+++ lib/generators/templates/config/initializers/cambium.rb
@@ -1,3 +1,22 @@
 Cambium.configure do |config|
-  config.say_hello = 'hello'
+
+  # -------------------------------------------------- Assets
+  # 
+  # The asset helpers define what Cambium will add to 
+  # your Gemfile. They also tell Cambium which 
+  # frameworks to install and configure.
+  # 
+  config.stylesheets_helpers = [:compass, :bones]
+  config.javascripts_helpers = [:backbone]
+
+  # -------------------------------------------------- Uploader
+  # 
+  # This is the gem to use to capture file uploads. 
+  # It helps Cambium build you Gemfile, and it tells 
+  # Cambium which config file to generate
+  # 
+  # Options: [:dragonfly, :carrierwave]
+  # 
+  config.uploader = :dragonfly
+
 end
