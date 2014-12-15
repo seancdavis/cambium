84b0789bd574d6dc655b0203fa6a42616c86d4a5
Sean C Davis <scdavis41@gmail.com>
2014-12-15 13:16:31 -0500

    indentation

--- lib/generators/cambium/install_generator.rb
+++ lib/generators/cambium/install_generator.rb
@@ -5,15 +5,15 @@ module Cambium
   class InstallGenerator < Rails::Generators::Base
     desc "Add Cambium config file to your initializers."
 
-      source_root File.expand_path('../../templates', __FILE__)
+    source_root File.expand_path('../../templates', __FILE__)
 
-      # Copy our Cambium config file into the project's config/initializers
-      # directory.
-      # 
-      def add_config_file
-        config_file = "config/initializers/cambium.rb"
-        copy_file config_file, config_file
-      end
+    # Copy our Cambium config file into the project's config/initializers
+    # directory.
+    # 
+    def add_config_file
+      config_file = "config/initializers/cambium.rb"
+      copy_file config_file, config_file
+    end
 
   end
 end
