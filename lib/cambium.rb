c1b1056016e89d5b2f7ab52b97fa96a6e9967333
Sean C Davis <scdavis41@gmail.com>
2014-12-15 12:11:13 -0500

    load configuration

--- lib/cambium.rb
+++ lib/cambium.rb
@@ -1,5 +1,22 @@
 require "cambium/version"
 
 module Cambium
-  require 'cambium/engine' if defined?(Rails)
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :say_hello
+
+    def initialize
+      @say_hello = 'cheers'
+    end
+  end
+
 end
