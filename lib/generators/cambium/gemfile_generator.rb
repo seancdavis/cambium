2526cd93c62b4d8bb667ebf270f0223cf5066d89
Sean C Davis <scdavis41@gmail.com>
2015-04-02 15:41:17 -0400

    remove several options and don't require the options to install default Gemfile

--- lib/generators/cambium/gemfile_generator.rb
+++ lib/generators/cambium/gemfile_generator.rb
@@ -8,50 +8,11 @@ module Cambium
 
     source_root File.expand_path('../../templates', __FILE__)
 
-    class_option(
-      :config_check,
-      :type => :boolean,
-      :default => true,
-      :description => "Verify config at config/initializers/cambium.rb"
-    )
-
-    # If there is no configuration file tell the user to run
-    # that generator first (unless user has manually
-    # overridden).
-    #
-    def verify_configuration
-      if options.config_check?
-        unless File.exists?("#{Rails.root}/config/initializers/cambium.rb")
-          help_message('cambium_prereqs')
-          exit
-        end
-      end
-    end
-
     # Here we figure out some of the more complicated logic
     # before rendering the Gemfile template
     #
     def resolve_options
-
-      # database adapter
-      db_adapter = ActiveRecord::Base.connection.instance_values["config"][:adapter]
-      case db_adapter
-      when 'sqlite3'
-        @db_gem = 'sqlite3'
-      when 'mysql'
-        @db_gem = 'mysql2'
-      when 'postgresql'
-        @db_gem = 'pg'
-      end
-
-      # rails version
       @rails_version = `bundle exec rails -v`.to_s.split(' ').last
-
-      # assets
-      @stylesheets = Cambium.configuration.stylesheets_helpers
-      @javascripts = Cambium.configuration.javascripts_helpers
-      @uploader = Cambium.configuration.uploader
-
     end
 
     # Move our default Gemfile to the project's Gemfile.
--- lib/generators/templates/config/initializers/cambium.rb
+++ lib/generators/templates/config/initializers/cambium.rb
@@ -2,38 +2,15 @@ Cambium.configure do |config|
 
   # -------------------------------------------------- App Info
 
-  # Cambium uses these to set your root urls for 
+  # Cambium uses these to set your root urls for
   # mailers and other fun stuff.
-  # 
+  #
   config.development_url = 'localhost:3000'
   config.production_url = 'example.com'
 
-  # The title of your app is what Cambium puts in the 
+  # The title of your app is what Cambium puts in the
   # browser tab by default.
-  # 
+  #
   config.app_title = 'Cambium'
-
-  # -------------------------------------------------- Assets
-
-  # The asset helpers define what Cambium will add to 
-  # your Gemfile. They also tell Cambium which 
-  # frameworks to install and configure.
-  # 
-  # NOTE: Only those listed below are available at this 
-  # time. All others should be added to your Gemfile 
-  # and configured manuallly.
-  # 
-  config.stylesheets_helpers = [:compass, :bones]
-  config.javascripts_helpers = [:backbone]
-
-  # -------------------------------------------------- Uploader
-
-  # This is the gem to use to capture file uploads. 
-  # It helps Cambium build you Gemfile, and it tells 
-  # Cambium which config file to generate
-  # 
-  # Options: [:dragonfly, :carrierwave]
-  # 
-  config.uploader = :dragonfly
 
 end
