c6cdd6d5add75af99f174e0549b326ea274022cb
Sean C Davis <scdavis41@gmail.com>
2016-01-07 15:56:19 -0500

    allow absence of plain page model

--- app/controllers/cambium/admin/pages_controller.rb
+++ app/controllers/cambium/admin/pages_controller.rb
@@ -3,9 +3,8 @@ class Cambium::Admin::PagesController < Cambium::AdminController
   private
 
     def create_params
-      p = params
-        .require(admin_model.to_s.tableize.singularize.to_sym)
-        .permit(admin_form.fields.to_h.keys)
+      obj = admin_model.to_s.gsub(/Cambium::/, '').tableize.singularize.to_sym
+      p = params.require(obj).permit(admin_form.fields.to_h.keys)
       if params[:page][:template_data].present?
         data = @object.template_data.merge(params[:page][:template_data])
         p = p.merge(:template_data => data)
