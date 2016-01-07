570bd64129d62ff05ca8d131d0254293caa13b25
Sean C Davis <scdavis41@gmail.com>
2016-01-07 11:18:28 -0500

    add migrations automatically to rails migrations

--- lib/cambium/engine.rb
+++ lib/cambium/engine.rb
@@ -12,5 +12,13 @@ module Cambium
       g.helper false
     end
 
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
   end
 end
