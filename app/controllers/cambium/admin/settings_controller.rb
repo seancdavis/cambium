6badbf7e35493f29b50f25bf577d55d3df5dc92a
Sean C Davis <scdavis41@gmail.com>
2016-01-08 15:11:52 -0500

    Remove settings that have been removed from the config (#37)

--- app/controllers/cambium/admin/settings_controller.rb
+++ app/controllers/cambium/admin/settings_controller.rb
@@ -3,11 +3,19 @@ class Cambium::Admin::SettingsController < Cambium::AdminController
   before_filter :not_found, :except => [:index, :update]
 
   def index
-    exp_settings = admin_view.form.edit.fields.to_h.stringify_keys.keys
-    (exp_settings - Cambium::Setting.keys).each do |key|
+    expected_keys = admin_view.form.edit.fields.to_h.stringify_keys.keys
+    current_keys = Cambium::Setting.keys
+    # Add anything that's missing
+    (expected_keys - current_keys).each do |key|
       Cambium::Setting.create(:key => key)
     end
     @collection = admin_model.alpha
+    # Remove anything extra
+    extra_keys = @collection.collect(&:key) - expected_keys
+    if extra_keys.size > 0
+      Cambium::Setting.where(:key => extra_keys).destroy_all
+      @collection = admin_model.alpha
+    end
     respond_to do |format|
       format.html
       format.csv { send_data admin.to_csv(@collection) }
