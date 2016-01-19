838719d39899a761cff719f70944ae19e3c7475f
Sean C Davis <scdavis41@gmail.com>
2016-01-19 13:03:47 -0500

    render the application controller for public pages

--- app/controllers/cambium/pages_controller.rb
+++ app/controllers/cambium/pages_controller.rb
@@ -3,7 +3,7 @@ class Cambium::PagesController < Cambium::BaseController
   def show
     slug = request.path.split('/').last
     @page = Cambium::Page.find_by_slug(slug)
-    render :inline => @page.template.content
+    render :inline => @page.template.content, :layout => 'application'
   end
 
   def home
@@ -11,7 +11,7 @@ class Cambium::PagesController < Cambium::BaseController
     if @page.nil?
       render 'home_missing'
     else
-      render :inline => @page.template.content
+      render :inline => @page.template.content, :layout => 'application'
     end
   end
 
