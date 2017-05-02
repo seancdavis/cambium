c0ad4bd21a7a44b772af2addc27726de7723f659
Sean C Davis <scdavis41@gmail.com>
2017-05-01 22:16:36 -0400

    Add a few more string patches

--- lib/cambium/support/string.rb
+++ lib/cambium/support/string.rb
@@ -4,4 +4,25 @@ class String
     return false if self == false || self.blank? || self =~ (/^(false|f|no|n|0)$/i)
     raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
   end
+
+  def possessive
+    self + ('s' == self[-1, 1] ? "'" : "'s")
+  end
+
+  def paragraphs
+    pattern = /\n/
+    split(pattern).map(&:strip).reject(&:blank?)
+  end
+
+  def sentences
+    pattern = /[\.\?\!]/
+    punctuation = scan(pattern)
+    sentences = split(pattern).map(&:strip).reject(&:blank?)
+    return sentences if punctuation.blank?
+    sentences.each_with_index do |txt, idx|
+      next if punctuation[idx].blank?
+      sentences[idx] += punctuation[idx]
+    end
+    sentences
+  end
 end
--- spec/lib/support/string_spec.rb
+++ spec/lib/support/string_spec.rb
@@ -14,4 +14,31 @@ describe String do
     end
   end
 
+  describe '#possessive' do
+    it 'adds an apostrophe when ending in s' do
+      expect('Davis'.possessive).to eq("Davis'")
+    end
+    it 'adds an apostrophe-S when not ending in s' do
+      expect('Sean'.possessive).to eq("Sean's")
+    end
+  end
+
+  describe '#paragraphs' do
+    it 'breaks on newlines' do
+      txt = "I\nAnd Love\nAnd You"
+      expect(txt.paragraphs).to eq(['I', 'And Love', 'And You'])
+    end
+    it 'removes extra newlines' do
+      txt = "I\n\nAnd Love\n\nAnd You"
+      expect(txt.paragraphs).to eq(['I', 'And Love', 'And You'])
+    end
+  end
+
+  describe '#sentences' do
+    it 'breaks on periods, exclamation marks, and question marks' do
+      txt = "I? And, Love. And You! And Us"
+      expect(txt.sentences).to eq(['I?', 'And, Love.', 'And You!', 'And Us'])
+    end
+  end
+
 end
